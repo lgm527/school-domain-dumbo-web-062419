396d24bfedf7fab201770c9ae1732a6c79a7ce01
lgm527 <laurellmccaffrey@gmail.com>
2019-06-25 18:55:35 -0400

    school domain created

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
 # code here!
+
+class School
+
+  def initialize(roster)
+    @roster = Hash.new
+
+    def roster
+      @roster
+    end
+
+  end
+
+  def add_student(student, grade)
+
+    if !@roster.include?(grade)
+      @roster[grade] = []
+      @roster[grade] << student
+    else
+      @roster[grade] << student
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |grade, students|
+      @roster[grade] = students.sort
+    end
+  end
+
+end
